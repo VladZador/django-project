7c419dc91059e131e70630e545141b1375a15151
Vladyslav Zadorozhnii <vladzador.ua@gmail.com>
2022-10-09 22:21:41 +0300

    Create items form

---
+++ feedbacks/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+# class Feedback(models.Model):
+#     text = models.TextField()
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     rating = models.PositiveSmallIntegerField()
